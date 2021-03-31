6a6f2471f2482fdba2cf182b31f0d1e2b237978f
naiyume <naiyume>
2021-03-30 21:42:27 -0400

    adding the git actions workflow

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+let files = fs.readFileSync('files.csv', {encoding:'utf8'})
+files = files.split(',')
+console.log(files)
+
+let changed = false;
+for(let i = 0; i < files.length; i++){
+    let file = files[i]
+    if(file == 'masterJSON.json'){
+        const fs = require('fs');
+        //clear files
+        fs.readdirSync('./jsons/').forEach(file => {
+            if(file.includes('.json')){
+                fs.unlink('./jsons/' + file, (err) => {
+                    if(err){
+                        console.error(err)
+                        return
+                    }
+                })        
+            }
+        })
+        
+        let currFileContents = fs.readFileSync('./masterJSON.json');
+        let currJSON = JSON.parse(currFileContents)
+        let fileNames = Object.keys(currFileContents);
+        for(let i = 0; i < fileNames.length; i++){
+            fs.writeFileSync(fileNames[i] + '.json', JSON.stringify(currJSON[fileNames[i]],null, 2));
+        }
+
+        changed = true;
+        i = files.length;
+    }
+    
+}
+/*
+if(changed){
+    aggregate.aggregate();
+}*/
